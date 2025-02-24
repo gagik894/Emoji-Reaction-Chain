dab7813480bc80c8456553e204ec6e6607ad69ff
gagik894 <gagiksargsyan894@gmail.com>
2025-02-24 17:28:24 +0100

    code clean up

--- app/src/main/java/com/play/emojireactionchain/utils/AnswerOptionGenerator.kt
+++
@@ -1,8 +0,0 @@
-package com.play.emojireactionchain.utils
-
-import com.play.emojireactionchain.viewModel.EmojiCategory
-import com.play.emojireactionchain.viewModel.GameRule
-
-interface AnswerOptionGenerator {
-    fun generateOptions(correctAnswerEmoji: String, category: EmojiCategory, rule: GameRule, emojiChain: List<String>, level: Int): List<String>
-}
--- app/src/main/java/com/play/emojireactionchain/utils/EmojiChainGenerator.kt
+++
@@ -1,7 +0,0 @@
-package com.play.emojireactionchain.utils
-
-import com.play.emojireactionchain.model.GeneratedChainData
-
-interface EmojiChainGenerator {
-    fun generateChain(availableEmojis: List<String>, level: Int): GeneratedChainData
-}
